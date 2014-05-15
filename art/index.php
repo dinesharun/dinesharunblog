7e4aa750397f6e09bee185c413d1eef750e43ff4
dinesharun <dnsh.arun@gmail.com>
2014-05-15 11:54:36 +0200

    Cleanup index for the art module.

--- art/index.php
+++ art/index.php
@@ -1,15 +1,9 @@
 <?php include("../template.php");
-
   AddHeader(1, "Art", "Blog, Photography, Blender, Nature, Landscape, Electronics, Animation");
-
   AddBody(1, 0, 0);
-
 ?>
 
-
 <?php
-
   AddFooter(1, 0, 0);
-
 ?>
 
