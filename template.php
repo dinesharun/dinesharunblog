9726a66ba220bc216f71db3ae58c890ac1278b25
dinesharun <dnsh.arun@gmail.com>
2014-05-21 16:42:38 +0200

    Rearranging template and getpage

--- template.php
+++ template.php
@@ -124,6 +124,25 @@
 	 echo '<div class="contentFrame" id="contentFrame">';
   }
 
+  function getPage($cat, $idx, $post)
+  {
+    /* Build the post path */
+    $postFile = "posts/" . $cat . "/" . $idx . "/" . $post . ".txt";
+	
+	/* Get the file contents */
+    $postData = file_get_contents($postFile);
+	
+	/* If got post the data */
+	if($postData != FALSE)
+	{
+	  echo $postData;
+	}
+	else
+	{
+	  echo "No Such Posts Exists!";
+	}
+  }
+  
   function AddBody($cat, $idx, $post)
   {
     global $pathPrefix;
@@ -276,6 +295,116 @@
     echo '</body>';
 
     echo '</html>';
+  }
+  
+  function GetRandomImages()
+  {
+    global $Debug;
+    $startPath = "images/art/photo";
+	$imgPath   = "";
+
+	$imagesCount = CountImagesInDir(1, $startPath, 0, 0, $imgPath);
+
+	if($Debug) { echo ' totalImages : ' . $imagesCount . '<br />'; }
+
+    $i = 0;
+    $selected = array();
+
+    echo '<div class="imgStrip">';
+
+    while($i < 4)
+    {
+      $currImgIndex = rand(1, $imagesCount);
+      $selectedIndx[$i] = $currImgIndex;
+
+      $j = 0;
+
+      /* Check for duplicates if there are enough to check */
+      while(($imagesCount >= 4) && ($j < $i))
+      {
+        /* If same index already selected */
+        if($selectedIndx[$j] == $currImgIndex)
+        {
+          /* Select a new one */
+          $currImgIndex = rand(1, $imagesCount);
+          $selectedIndx[$i] = $currImgIndex;
+          $j = 0;
+        }
+        else
+        {
+          $j++;
+        }
+      }
+
+      if($Debug) { echo ' currImgIndex : ' . $currImgIndex . '&nbsp;'; }
+
+      $imagesCount = CountImagesInDir(1, $startPath, 0, $currImgIndex, $imgPath);
+
+      echo '<img id="randImages' . $i .'" style="width: 21%; cursor:pointer; margin-left: 1.5%; margin-right: 1.5%; margin-top: 0%; margin-bottom: 0%; border: 2px double grey;" src="'. $imgPath .'" onLoad="ResizeImage(' . $i . ')" onClick="ShowImage(\'' . str_replace("thumbs/", "", $imgPath) . '\', \'Image of the Moment\')" />';
+
+      $i++;
+    }
+
+	echo '</div>';
+  }
+  
+  function countImagesInDir($firstCall, $path, $thumbDir, $imageIndx, &$selectedFile)
+  {
+    global $Debug;
+	global $totalImages;
+	$newPath = "";
+
+    if($firstCall == 1)
+    {
+      $totalImages = 0;
+    }
+
+	if(is_dir($path))
+	{
+	  if($currDir = opendir($path))
+	  {
+		while(($file = readdir($currDir)) != false)
+        {
+		  $newPath = $path . '/' . $file;
+		  if($Debug)
+		   { echo "filename:" . $file . ", filetype: " . filetype($newPath) . "<br />"; }
+
+		  if((filetype($newPath) == 'dir') && (($file != '.') && ($file != '..')))
+		  {
+		    if($file == "thumbs")
+			{
+              countImagesInDir(0, $newPath, 1, $imageIndx, $selectedFile);
+			}
+			else
+			{
+              countImagesInDir(0, $newPath, 0, $imageIndx, $selectedFile);
+			}
+		  }
+		  else
+		  {
+		    if(((filetype($newPath) == 'file')) && ($thumbDir == 1))
+			{
+			  if((substr_count($newPath, '.jpg') > 0) ||
+			     (substr_count($newPath, '.JPG') > 0))
+			  {
+		        $totalImages++;
+
+				if($totalImages == $imageIndx)
+				{
+				  $selectedFile = $newPath;
+				}
+
+				if($Debug)
+				{ echo ' Selected File : ' . $newPath . '<br />'; }
+		      }
+			}
+		  }
+		}
+	  }
+	  closedir($currDir);
+    }
+
+	return $totalImages;
   }
   
   function AddCommentSection($cat, $idx, $post)
