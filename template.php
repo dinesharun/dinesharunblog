4c590c9cf4636ecafa8fe32be162ccbe96519048
dinesharun <dnsh.arun@gmail.com>
2014-05-26 10:17:25 +0200

    Fix for the file system inaccessible GAE issue

--- template.php
+++ template.php
@@ -354,6 +354,9 @@
 	global $totalImages;
 	$newPath = "";
 
+	/* Until GAE Support file system access or 
+	   until an alternate mathod is found
+	--------------------------------------------
     if($firstCall == 1)
     {
       $totalImages = 0;
@@ -411,6 +414,18 @@
 	{
 	  if($Debug) { echo "Path:" . $path . "is not a Dir"; }
 	}
+	--------------------------------------------------------*/
+	
+	$totalImages = 21;
+	$randImgs    = array('images/art/photo/Flowers/images/thumbs/DSC00025.JPG', 'images/art/photo/Flowers/images/thumbs/DSC00039.JPG', 'images/art/photo/Flowers/images/thumbs/DSC00008.JPG',
+	                     'images/art/photo/Flowers/images/thumbs/DSC09158.JPG', 'images/art/photo/Flowers/images/thumbs/DSC09048.JPG', 'images/art/photo/Flowers/images/thumbs/DSC01083.JPG',
+						 'images/art/photo/rain/images/thumbs/DSC01142.JPG',    'images/art/photo/rain/images/thumbs/DSC07569.JPG',    'images/art/photo/rain/images/thumbs/DSC08640.JPG',
+						 'images/art/photo/seasons/images/thumbs/DSC01347.jpg', 'images/art/photo/seasons/images/thumbs/DSC01349.jpg', 'images/art/photo/seasons/images/thumbs/DSC01368.jpg',
+						 'images/art/photo/seasons/images/thumbs/DSC01390.jpg', 'images/art/photo/seasons/images/thumbs/DSC01419.jpg', 'images/art/photo/seasons/images/thumbs/DSC01433.jpg',
+						 'images/art/photo/sunriseset/images/thumbs/DSC01231.JPG', 'images/art/photo/sunriseset/images/thumbs/DSC01419.JPG', 'images/art/photo/sunriseset/images/thumbs/DSC01427.JPG',
+						 'images/art/photo/trees/images/thumbs/DSC01533.JPG', 'images/art/photo/trees/images/thumbs/DSC00012.JPG', 'images/art/photo/trees/images/thumbs/DSC03663.JPG');
+						 
+    $selectedFile = $randImgs[$imageIndx];				 
 
 	return $totalImages;
   }
