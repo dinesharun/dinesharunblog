c687b016fd7f017c44d6b5c5bd621613a52fab79
dinesharun <dnsh.arun@gmail.com>
2014-06-12 11:40:43 +0200

    use correct null

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ function StartScripts()
   formatCodeBlock();
   
   window.addEventListener("popstate", function(e) {
-    if(e.state == NULL)
+    if(e.state == null)
     {
       getPage(0, 0, 0, false);
     }
