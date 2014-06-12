4b462f29d6b992979459888fd2e425a10abe6528
dinesharun <dnsh.arun@gmail.com>
2014-06-12 11:37:37 +0200

    bug fix

--- getpage.php
+++ getpage.php
@@ -27,7 +27,7 @@
     $link = AddBody($catId, $idxId, $postId);
     
     /* Return the link of the current page as a property in a empty DIV */
-    echo '<div id="currLinkDiv" class="currLinkDiv" linkStr="' . $link . '" titleStr="' . $postId . '" ></div>';
+    echo '<div><div id="currLinkDiv" class="currLinkDiv" linkStr="' . $link . '" titleStr="' . $postId . '" ></div></div>';
   }
   else if(isset($_GET["randImages"]))
   {
