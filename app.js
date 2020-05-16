49c4696e8107404c3d3bc50aff7bae3e6623a796
Dinesh Arun <dnsh.arun@gmail.com>
2020-05-16 22:04:50 +0200

    Add first support for google login

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 'use strict';
 
 const express = require('express');
+const session = require('express-session');
+const cookie = require('cookie-parser');
+const admin = require('./admin/admin.js');
+
 const app = express();
+app.use(cookie());
+app.use(express.json());
+app.use(session({secret: "replacethissecret"}));
 
 app.use("/css", express.static(__dirname + '/css'));
 app.use("/imgs", express.static(__dirname + '/imgs'));
 app.use("/scripts", express.static(__dirname + '/scripts'));
+
+app.get("/admin", (req, res) => {
+  admin.runQuery(req, res);
+});
+
+app.post("/admin/doLogin", (req, res) => {
+  admin.doLogin(req, res);
+});
 
 app.get('/*', (req, res) => {
   res.sendFile(__dirname + '/index.html');
