27103f9127c504cda18ddea46f1b38ab8585efc8
Dinesh Arun <dnsh.arun@gmail.com>
2020-07-04 23:44:54 +0200

    Improvements to add post list and post

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.use("/scripts", express.static(__dirname + '/scripts'));
 /* Initialize the admin module */
 admin.initApp(app, express);
 
+/* For the favicon */
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(__dirname + '/favicon.ico');
+});
+
 /* For root route to home page */
 app.get('/*', (req, res) => {
   res.sendFile(__dirname + '/index.html');
--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,177 @@
+const insQuotes = [
+  'You dont take a photograph, you make it <br /> - Ansel Adams',
+  'There are no rules for good photographs, there are only good photographs. <br /> - Ansel Adams',
+  'A photograph is a portrait painted by the sun <br /> - unknown',
+  'A work of art which did not begin in emotion is not art <br /> - Paul Cezanne',
+  'Great art picks up where nature ends <br /> - Marc Chagall',
+  'Art is not what you see, but what you make others see <br /> - Edgar Degas',
+  'A Good artist has less time than ideas <br /> - Martin Kippenberger',
+  'I saw the angel in the marble and carved until I set him free <br /> - Michelangelo'
+];
+
 /* Onload init function */
 function onLoadFn() {
   new Vue({
         el: "#vueApp",
         data: {
-          loading: true,
           currLevel: -1,
-          postData: {},
-          postValid: false
+          urlParts: [],
+          loading: true,
+          postList: [],
+          listReq: false,
+          postData: [],
+          dataReq: false,
+          lastErr: -1
         },
         methods: {
-          onLoad: function() {
-            var urlParts = window.location.pathname.split("/");
-            urlParts = urlParts.filter(f => (f != ""));
-            this.currLevel = urlParts.length;
-
-            /* load root links */
-            if(this.currLevel == 0) {
-              this.loading = false;
-            /* load category links */
-            } else if(this.currLevel == 1) {
-              this.loading = false;
-            /* load the post */
+          loadUrl: function(url, pushSt=true) {
+            /* If the state is to be pushed */
+            if(pushSt == true) {
+              /* Push current URL to history */
+              window.history.pushState(null, null, url);
+            }
+            
+            /* Clear data */
+            this.postList.length = 0;
+            this.postData.length = 0;
+            this.lastErr = -1;
+            this.currLevel = -1;
+              
+            /* Check if this is a specifc post request or a intermediate path request */
+            if((url == "") || (url.charAt(url.length - 1) == '/')) {
+              this.listReq = true;
+              this.dataReq = false;
+              document.title = "Art & Technology";
             } else {
+              this.listReq = false;
+              this.dataReq = true;
+            }
+            
+            /* Find out the number of the levels in the path */
+            this.urlParts = url.split("/");
+            this.urlParts = this.urlParts.filter(f => (f != ""));
+            this.currLevel = this.urlParts.length;
+            
+            /* Load the data */
+            this.loadData();
+          },
+          loadPost: function(post) {
+            var url = "/" + post.cats[0] + "/" + post.url;
+            this.loadUrl(url);
+          },
+          getDateTimeStr: function(ts) {
+            var dt = new Date(ts);
+            const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+            const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+            const h = dt.getHours();
+            const m = dt.getMinutes();
+            
+            var str = days[dt.getDay()] + ', ' + dt.getDate() + ' ' + months[dt.getMonth()] + ' ' + dt.getFullYear() + ', ';
+            str += ((h < 10) ? ('0' + h):(h));
+            str += ':' + ((m < 10) ? ('0' + m):(m));
+            
+            return str;
+          },
+          popState: function(e) {
+            this.loadUrl(window.location.pathname, false);
+          },
+          getThumbUrl: function(tUrl) {
+            if((tUrl == "") || (tUrl == undefined)) {
+              return "/imgs/gears.png";
+            } else {
+              return tUrl;
+            }
+          },
+          getErrStr: function() {
+            var str = "";
+            switch(this.lastErr) {
+              case 1:
+                str = "No posts for this category";
+                break;
+              case 2:
+                str = "Requested post not found";
+                break;
+              default:
+                str = "HTTP Error " + this.lastErr;
+                break;
+            }
+            return str;
+          },
+          loadData: function() {
+            /* load lists */
+            if(this.listReq == true) {
+              this.loading = true;
               var vueObj = this;
               var xhr = new XMLHttpRequest();
-              xhr.open("GET", ("/post" + window.location.pathname));
+              var filReq = "";
+              this.urlParts.forEach(function(u, i) {
+                filReq += (((i != 0)?(','):('cat=')) + u);
+              });
+              xhr.open("GET", ("/getPostList?" + filReq));
               xhr.onreadystatechange = function() {
                 if(this.readyState == 4) {
-                  if(this.status == 200) {
-                    vueObj.postValid = true;
-                    vueObj.postData = JSON.parse(this.responseText);
+                  if((this.status == 200) || (this.status == 304)) {
+                    vueObj.postList = JSON.parse(this.responseText);
+                    if(vueObj.postList.length > 0) {
+                      vueObj.lastErr = 0;
+                    } else {
+                      vueObj.lastErr = 1;
+                    }
+                  } else {
+                    vueObj.lastErr = this.status;
                   }
                   vueObj.loading = false;
                 } 
               }
               xhr.send(); 
+            /* load post */
+            } else if(this.dataReq == true) {
+              this.loading = true;
+              var vueObj = this;
+              var xhr = new XMLHttpRequest();
+              xhr.open("GET", ("/post" + window.location.pathname));
+              xhr.onreadystatechange = function() {
+                if(this.readyState == 4) {
+                  if((this.status == 200) || (this.status == 304)) {
+                    vueObj.postData = JSON.parse(this.responseText);
+                    if(vueObj.postData.length > 0) {
+                      vueObj.lastErr = 0;
+                      document.title = vueObj.postData[0].title;
+                    } else {
+                      vueObj.lastErr = 2;
+                    }
+                  } else {
+                    vueObj.lastErr = this.status;
+                  }
+                  vueObj.loading = false;
+                } 
+              }
+              xhr.send(); 
+            } else {
+              /* Do nothing */
             }
           }
         },
+        computed: {
+          randomQuote: function() {
+            var rStr = "";
+            
+            if((this.loading == false) && (this.lastErr == 0)) {
+              var rIdx = Math.round(Math.random() * insQuotes.length);
+              rStr = insQuotes[rIdx];
+            }
+            
+            return rStr;
+          }
+        },
         mounted: function() {
-          this.onLoad();
+          this.loadUrl(window.location.pathname);
+        },
+        created: function() {
+          window.addEventListener('popstate',this.popState);
+        },
+        destroyed: function() {
+          window.removeEventListener('popstate', this.popState);
         }
   });
 }
