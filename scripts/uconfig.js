7e92922e48b27118a709b605aa825cd261f1d2e0
Dinesh Arun <dnsh.arun@gmail.com>
2020-12-30 18:46:15 +0100

    Move to use the common client scripts from admin

---
+++ scripts/uconfig.js
@@ -0,0 +1,12 @@
+const insQuotes = [
+  'You dont take a photograph, you make it <br /> - Ansel Adams',
+  'There are no rules for good photographs, there are only good photographs. <br /> - Ansel Adams',
+  'A photograph is a portrait painted by the sun <br /> - unknown',
+  'A work of art which did not begin in emotion is not art <br /> - Paul Cezanne',
+  'Great art picks up where nature ends <br /> - Marc Chagall',
+  'Art is not what you see, but what you make others see <br /> - Edgar Degas',
+  'A Good artist has less time than ideas <br /> - Martin Kippenberger',
+  'I saw the angel in the marble and carved until I set him free <br /> - Michelangelo'
+];
+const docTitle = "Art & Technology";
+const homeRandGallery = true;
